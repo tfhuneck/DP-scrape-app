a9e68f1e55a02b04952d963e15a88e00f4d1e9d0
Timothy Huneck <tfhuneck@ucdavis.edu>
2023-02-01 14:56:49 -0800

    node modules and chromedriver command added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const webdriver = require('chromedriver');
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 // const   webdriver = require("selenium-webdriver"), 
