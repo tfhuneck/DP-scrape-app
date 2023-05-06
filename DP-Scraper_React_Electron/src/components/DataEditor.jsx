814cc867a431febf07c9006a815a9827d2625ef1
Timothy Huneck <timothyhuneck@Timothys-MBP.lan>
2023-05-06 12:05:26 -0700

    data editor and api updates

--- DP-Scraper_React_Electron/src/components/DataEditor.jsx
+++ DP-Scraper_React_Electron/src/components/DataEditor.jsx
@@ -1,50 +1,151 @@
-import { basketballdata } from '../basketballdata';
 import { useState, useEffect } from 'react';
 
+const table = (dataset, title) => {
+    return (
+        <>
+        <h1>{title}</h1>
+        <table className='table table-light table-striped table-hover'>
+            <thead>
+                <tr>
+                    <th scope="col">Product Name</th>
+                    <th scope="col">D&P</th>
+                    <th scope="col">Blowout</th>
+                    <th scope="col">Dave&Adams</th>
+                    <th scope="col">Steel City</th>
+                    <th scope="col">Rbi Cru7</th>
+                    <th scope="col">Edit</th>
+                </tr>
+            </thead>
+            <tbody>
+            {dataset.map((data, key) => {
+                return (
+                    <>
+                    <tr key={key}>
+                        <td className='pname'>{data.name}</td>
+                        <td className='data-list'>{data.dandp}</td>
+                        <td className='data-list'>{data.blowout}</td>
+                        <td className='data-list'>{data.dave}</td>
+                        <td className='data-list'>{data.steel}</td>
+                        <td className='data-list'>{data.rbi}</td>
+                        <td className='data-edit'>
+                            <button className="btn btn-sm btn-outline-dark dropdown-toggle" data-bs-toggle="dropdown">Edit</button>
+                            <ul className="dropdown-menu">
+                                <li><a key="edit" className="dropdown-item">Edit</a></li>
+                                <li><a key="delete" className="dropdown-item link-danger">Delete</a></li>
+                            </ul>
+                        </td>
+                    </tr>
+                    </>  
+                )
+            })}
+            </tbody>
+        </table> 
+        </>
+    )
+}
 
 function DataEditor() {
-    
 
+    const [basketballdata, setBasketballdata] = useState([]);
+    const [baseballdata, setBaseballdata] = useState([]);
+    const [footballdata, setFootballdata] = useState([]);
+    const [otherdata, setOtherdata] = useState([]);
+    const [displayTable, setDisplayTable] = useState('Basketball');
+    
+    const changeTable = (e) => {
+        let choice = e.target.innerHTML;
+        console.log(choice);
+        setDisplayTable(choice);
+    }
+
+    useEffect(() => {
+        window.getBasketballApi.getData()
+        .then((response) => {
+            console.log(response);
+            setBasketballdata(response);
+        })  
+        .catch((error) => {
+            console.error(error);
+          });
+    },[])
+
+    useEffect(() => {
+        window.getBaseballApi.getData()
+        .then((response) => {
+            console.log(response);
+            setBaseballdata(response);
+        })  
+        .catch((error) => {
+            console.error(error);
+          });
+    },[])
+
+    useEffect(() => {
+        window.getFootballApi.getData()
+        .then((response) => {
+            console.log(response);
+            setFootballdata(response);
+        })  
+        .catch((error) => {
+            console.error(error);
+          });
+    },[])
+
+    useEffect(() => {
+        window.getOtherApi.getData()
+        .then((response) => {
+            console.log(response);
+            setOtherdata(response);
+        })  
+        .catch((error) => {
+            console.error(error);
+          });
+    },[])
+    
+    const BasketballTable = () => {
+        if (displayTable == 'Basketball'){
+            return table(basketballdata, 'Basketball');
+        } else {
+            return
+        };
+    } 
+    const BaseballTable = () => {
+        if (displayTable == 'Baseball') {
+          return table(baseballdata, 'Baseball');
+        } else {
+            return
+        };
+    } 
+    const FootballTable = () => {
+        if (displayTable == 'Football') {
+           return table(footballdata, 'Football');
+        } else {
+            return
+        };
+    } 
+    const OtherTable = () => {
+        if (displayTable == 'Other') {
+            return table(otherdata, 'Other');
+        } else {
+            return
+        };
+    } 
+    
     return (
         <>
             <div className='container-fluid'>
+                <div className="btn-group">
+                        <button onClick={changeTable} className="btn btn-outline-dark">Basketball</button>
+                        <button onClick={changeTable} className="btn btn-outline-dark">Baseball</button>
+                        <button onClick={changeTable} className="btn btn-outline-dark">Football</button>
+                        <button onClick={changeTable} className="btn btn-outline-dark">Other</button>
+                </div>
+               <br/><br/>
                 <div className='row'>
-                    <table className='table table-light table-striped table-hover'>
-                        <thead>
-                            <tr>
-                                <th scope="col">Product Name</th>
-                                <th scope="col">D&P</th>
-                                <th scope="col">Blowout</th>
-                                <th scope="col">Dave&Adams</th>
-                                <th scope="col">Steel City</th>
-                                <th scope="col">Rbi Cru7</th>
-                                <th scope="col">Edit</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {basketballdata.map((data, key) => {
-                            return (
-                                <>
-                                <tr key={key}>
-                                    <td className='pname'>{data.name}</td>
-                                    <td className='data-list'>{data.dandp}</td>
-                                    <td className='data-list'>{data.blowout}</td>
-                                    <td className='data-list'>{data.dave}</td>
-                                    <td className='data-list'>{data.steel}</td>
-                                    <td className='data-list'>{data.rbi}</td>
-                                    <td className='data-edit'>
-                                        <button className="btn btn-sm btn-outline-dark dropdown-toggle" data-bs-toggle="dropdown">Edit</button>
-                                        <ul className="dropdown-menu">
-                                            <li><a key="edit" className="dropdown-item">Edit</a></li>
-                                            <li><a key="delete" className="dropdown-item link-danger">Delete</a></li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                </>  
-                            )
-                        })}
-                        </tbody>
-                    </table> 
+                    <BasketballTable/>
+                    <BaseballTable/>
+                    <FootballTable/>
+                    <OtherTable/>
                 </div>
             </div>
         </>
